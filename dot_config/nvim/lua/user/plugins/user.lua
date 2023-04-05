06be9ae17f2419e3e36287966449846ebf9d53ec
becelli <gustavobecelli@gmail.com>
2023-04-05 18:12:32 -0300

    moving to astrovim

---
+++ dot_config/nvim/lua/user/plugins/user.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "eddyekofo94/gruvbox-flat.nvim",
+    config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_flat_style = "hard"
+      vim.g.gruvbox_flat_transparent_background = true
+    end,
+  },
+}
