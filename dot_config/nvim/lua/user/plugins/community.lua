87cb531270c6d62eba3c445fc781a2448f7ed79a
becelli <gustavobecelli@gmail.com>
2023-04-27 20:59:25 -0300

    feat: add tmux ressurect

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -1,21 +1,15 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.color.tint-nvim" },
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
-  -- { import = "astrocommunity.markdown-and-latex.vimtex" },
-  -- { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline/" },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline/" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.editing-support.treej" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
-  -- { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
@@ -28,5 +22,10 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.motion.harpoon" },
-  { import = "astrocommunity.terminal-integration.vim-tpipeline" },
+  -- { import = "astrocommunity.terminal-integration.vim-tpipeline" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+
 }
