29123750b9d87f7c38461db471f87e29b06d65f6
becelli <gustavobecelli@gmail.com>
2023-05-09 16:41:25 -0300

    feat: update astronvim

--- dot_config/nvim/lua/user/plugins/user.lua
+++ dot_config/nvim/lua/user/plugins/user.lua
@@ -1,9 +1,19 @@
 return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
   {
-    "eddyekofo94/gruvbox-flat.nvim",
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
     config = function()
-      vim.o.background = "dark"
-      vim.g.gruvbox_flat_style = "hard"
+      require("lsp_signature").setup()
     end,
   },
+  -- {
+  --   "eddyekofo94/gruvbox-flat.nvim",
+  --   config = function()
+  --     -- vim.o.background = "dark"
+  --     vim.g.gruvbox_flat_style = "hard"
+  --   end,
+  -- },
 }
