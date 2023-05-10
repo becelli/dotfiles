cb293795c9a781c106c73e5b3b36a793d86820bc
becelli <gustavobecelli@gmail.com>
2023-05-10 00:04:49 -0300

    feat: update neovim

--- dot_config/nvim/lua/user/plugins/user.lua
+++ dot_config/nvim/lua/user/plugins/user.lua
@@ -9,11 +9,11 @@ return {
       require("lsp_signature").setup()
     end,
   },
-  -- {
-  --   "eddyekofo94/gruvbox-flat.nvim",
-  --   config = function()
-  --     -- vim.o.background = "dark"
-  --     vim.g.gruvbox_flat_style = "hard"
-  --   end,
-  -- },
+  {
+    "eddyekofo94/gruvbox-flat.nvim",
+    config = function()
+      -- vim.o.background = "dark"
+      vim.g.gruvbox_flat_style = "hard"
+    end,
+  },
 }
