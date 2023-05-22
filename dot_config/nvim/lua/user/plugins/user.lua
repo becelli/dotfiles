604210ed4761cacba7a6c9c62332db0b17473683
becelli <gustavobecelli@gmail.com>
2023-05-22 08:15:12 -0300

    fix cmp

--- dot_config/nvim/lua/user/plugins/user.lua
+++ dot_config/nvim/lua/user/plugins/user.lua
@@ -1,7 +1,4 @@
 return {
-	-- You can also add new plugins here as well:
-	-- Add plugins, the lazy syntax
-	-- "andweeb/presence.nvim",
 	{
 		"ray-x/lsp_signature.nvim",
 		event = "BufRead",
@@ -17,73 +14,55 @@ return {
 		end,
 	},
 	{
-		"sainnhe/gruvbox-material",
-		enabled = true,
-		priority = 1000,
-		config = function()
-			vim.o.background = "dark"
-			vim.g.gruvbox_material_background = "hard"
-			vim.cmd.colorscheme("gruvbox-material")
+		-- override nvim-cmp plugin
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-emoji", -- add cmp source as dependency of cmp
+		},
+		-- override the options table that is used in the `require("cmp").setup()` call
+		opts = function(_, opts)
+			-- opts parameter is the default options table
+			-- the function is lazy loaded so cmp is able to be required
+			local cmp = require("cmp")
+			-- modify the sources part of the options table
+			opts.sources = cmp.config.sources({
+				{ name = "nvim_lsp", priority = 1000 },
+				{ name = "luasnip", priority = 750 },
+				{ name = "buffer", priority = 300 },
+				{ name = "path", priority = 200 },
+				{ name = "emoji", priority = 300 },
+			})
+
+			-- add the category of the item (function, variable, etc.) to the completion menu
+			-- opts.formatting = {
+			-- 	format = function(entry, vim_item)
+			-- 		vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+			-- 		vim_item.menu = ({
+			-- 			nvim_lsp = "[LSP]",
+			-- 			luasnip = "[LuaSnip]",
+			-- 			buffer = "[Buffer]",
+			-- 			path = "[Path]",
+			-- 			emoji = "[Emoji]",
+			-- 		})[entry.source.name]
+			-- 		return vim_item
+			-- 	end,
+			-- }
+
+			opts.formatting = {
+				fields = { "menu", "abbr", "kind" },
+				format = function(entry, item)
+					local menu_icon = {
+						nvim_lsp = "",
+						luasnip = "",
+						buffer = "",
+						path = "",
+					}
+					item.menu = menu_icon[entry.source.name]
+					return item
+				end,
+			}
+
+			return opts
 		end,
 	},
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	enabled = true,
-	-- 	lazy = false,
-	-- 	priority = 150,
-	-- 	name = "catppuccin",
-	-- 	config = function()
-	-- 		require("catppuccin").setup({
-	-- 			background = {
-	-- 				light = "latte",
-	-- 				dark = "mocha",
-	-- 			},
-	-- 			styles = {
-	-- 				comments = { "italic" },
-	-- 				conditionals = {},
-	-- 				loops = {},
-	-- 				functions = {},
-	-- 				keywords = {},
-	-- 				strings = {},
-	-- 				variables = {},
-	-- 				numbers = {},
-	-- 				booleans = {},
-	-- 				properties = {},
-	-- 				types = {},
-	-- 				operators = {},
-	-- 			},
-	-- 			transparent_background = false,
-	-- 			show_end_of_buffer = false,
-	-- 			custom_highlights = function(colors)
-	-- 				return {
-	-- 					NormalFloat = { bg = colors.crust },
-	-- 					FloatBorder = { bg = colors.crust, fg = colors.crust },
-	-- 					VertSplit = { bg = colors.base, fg = colors.surface0 },
-	-- 					CursorLineNr = { fg = colors.mauve },
-	-- 					Pmenu = { bg = colors.crust, fg = "" },
-	-- 					PmenuSel = { bg = colors.surface0, fg = "" },
-	-- 					TelescopeSelection = { bg = colors.surface0 },
-	-- 					TelescopePromptCounter = { fg = colors.mauve },
-	-- 					TelescopePromptPrefix = { bg = colors.surface0 },
-	-- 					TelescopePromptNormal = { bg = colors.surface0 },
-	-- 					TelescopeResultsNormal = { bg = colors.mantle },
-	-- 					TelescopePreviewNormal = { bg = colors.crust },
-	-- 					TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-	-- 					TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-	-- 					TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
-	-- 					TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
-	-- 					TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
-	-- 					TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
-	-- 					IndentBlanklineChar = { fg = colors.surface0 },
-	-- 					IndentBlanklineContextChar = { fg = colors.surface2 },
-	-- 					GitSignsChange = { fg = colors.peach },
-	-- 					NvimTreeIndentMarker = { link = "IndentBlanklineChar" },
-	-- 					NvimTreeExecFile = { fg = colors.text },
-	-- 				}
-	-- 			end,
-	-- 		})
-	--
-	-- 		vim.api.nvim_command("colorscheme catppuccin")
-	-- 	end,
-	-- },
 }
