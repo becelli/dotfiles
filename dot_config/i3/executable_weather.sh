4067d4140561dadde62476b19ee21c71590239e0
Becelli <gustavobecelli@gmail.com>
2023-08-02 17:24:59 -0300

    2023-08-02

--- dot_config/i3/executable_weather.sh
+++ dot_config/i3/executable_weather.sh
@@ -12,10 +12,10 @@ i3status | (
 
         string_to_show=""
         # Get weather information from a cache file and format it
-        weather_input=$(cat ~/.weather.cache | cut -d ':' -f 2)
-        temperature=$(echo $weather_input | cut -d ' ' -f 2)
-        wind_speed=$(echo $weather_input | cut -d ' ' -f 3)
-        weather="$temperature | $wind_speed"
+        # weather_input=$(cat ~/.weather.cache | cut -d ':' -f 2)
+        # temperature=$(echo $weather_input | cut -d ' ' -f 2)
+        # wind_speed=$(echo $weather_input | cut -d ' ' -f 3)
+        # weather="$temperature | $wind_speed"
 
         # Get pomodoro status and format it
         pomodoro_input=$(i3-gnome-pomodoro status | cut -d ' ' -f 2)
@@ -25,7 +25,8 @@ i3status | (
             pomodoro="🍅 $pomodoro_input"
         fi
 
-        string_to_show="$pomodoro | $weather"
+        # string_to_show="$pomodoro | $weather"
+        string_to_show="$pomodoro"
         # Concatenate weather and pomodoro information and add it to the i3status output
         # concatenation="$pomodoro |$weather"
         # echo ",[{\"full_text\":\"$pomodoro\" },${line#,\[}" || exit
